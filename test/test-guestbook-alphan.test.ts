ec7d22fc6b3756904d97a31e1dffb59979453324
alphank <alphankaracaer@hotmail.com>
2022-08-07 14:35:17 +0300

    Initial commit

---
+++ test/test-guestbook-alphan.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as TestGuestbookAlphan from '../lib/test-guestbook-alphan-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/test-guestbook-alphan-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TestGuestbookAlphan.TestGuestbookAlphanStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
